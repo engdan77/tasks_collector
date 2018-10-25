66b2d06c99388857728cb5a4da71547c31df573b
Daniel Engvall <daniel@engvalls.eu>
2018-10-25 20:39:02 +0200

    tasksconverter package created

---
+++ src/tasksconverter/__init__.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+"""tasksconverter: ...."""
+
+__author__ = "Daniel Engvall"
+__email__ = "daniel@engvalls.eu"
+
+
+def format_subject(subject):
+    import re
+    # Highlight keywords
+    subject = re.sub('(@\w+\([^\]]+\))', '<b>\\1</b>', subject)
+    return subject
+
+
+def convert_date_attribute(date):
+    import datetime
+    if type(date) is datetime.datetime:
+        date = date.strftime('%Y-%m-%d')
+    else:
+        date = None
+    return date
+
+
+# noinspection PyUnboundLocalVariable,PyUnboundLocalVariable
+def to_generic(tasks_list, _type='outlook'):
+    generic_list = list()
+    for task in tasks_list:
+        if _type == 'outlook':
+            subject = format_subject(task['taskName'])
+            categories = task['taskCategories']
+            categories = parse_category(categories)
+            client, category = categories['client'], categories['category']
+            due_date = convert_date_attribute(task['due'])
+            start_date = convert_date_attribute(task['startDate'])
+            modified_date = convert_date_attribute(task['modifiedDate'])
+            close_date = convert_date_attribute(task['completeDate'])
+            if close_date:
+                status = 'close'
+            else:
+                status = 'open'
+        generic_task = {'subject': subject,
+                        'client': client,
+                        'category': category,
+                        'start_date': start_date,
+                        'close_date': close_date,
+                        'due_date': due_date,
+                        'modified_date': modified_date,
+                        'status': status}
+        generic_list.append(generic_task)
+    # sort the list
+    generic_list = sorted(generic_list, key=lambda x: (str(x['client']), str(x['category']), str(x['status'])))
+    return generic_list
+
+
+def parse_category(category_list, _type='outlook'):
+    """
+    Returns a dict with type of items
+    :param _type:
+    :param category_list:
+    :return:
+    """
+    import re
+
+    if _type == 'outlook':
+        rules = {'client': '\(([^\}]+)\)',
+                 'category': '\{([^\}]+)\}'}
+        result = {}
+        for _type in rules.keys():
+            result.update({_type: None})
+        for category in category_list:
+            for _type in rules.keys():
+                match = re.match(rules[_type], category)
+                if match:
+                    result.update({_type: match.group(1)})
+        return result
