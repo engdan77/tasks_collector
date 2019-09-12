05409df662f0a55646a01bb27cb5488f3f1d678d
Daniel Engvall <daniel.engvall@ipsoft.com>
2019-09-12 11:57:22 +0200

    Updating setup

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='tasks_collector',
     version='0.9.0',
     packages=find_packages(),
+    install_requires=requirements,
     url='https://github.com/engdan77/tasks_collector',
     license='MIT',
     author='Daniel Engvall',
