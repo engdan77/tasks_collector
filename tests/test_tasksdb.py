68bb372f3e4f8d8d1f561a25747203128b35ee37
Daniel Engvall <daniel@engvalls.eu>
2019-11-19 09:44:58 +0100

    Supply unittest for outlook

--- tests/test_tasksdb.py
+++ tests/test_tasksdb.py
@@ -1,6 +1,7 @@
-import os
+from datetime import date as d
 from pathlib import Path
 from tasks_collector.tasksdb.api import get_default_db_path, insert_or_updates_tasks
+from tasks_collector.tasksdb.api import OpenDB, cleanup
 import peewee as pw
 import pytest
 
@@ -14,9 +15,11 @@ def test_get_default_db_path(monkeypatch, tmp_path):
 @pytest.fixture
 def setup_db():
     db = pw.SqliteDatabase(':memory:')
+
     class Base(pw.Model):
         class Meta:
             database = db
+
     class Task(Base):
         subject = pw.CharField(unique=True)
         client = pw.CharField(null=True)
@@ -26,24 +29,55 @@ def setup_db():
         due_date = pw.DateField(null=True)
         modified_date = pw.DateField(null=True)
         status = pw.CharField(null=True)
+
     db.connect()
     db.create_tables([Task], safe=True)
     return db, Task
 
 
-def test_insert_or_updates_tasks(setup_db, mocker):
-    t1 = {'subject': '[xxx-656] Helping xxx with escalations after xxx-Dev upgrade to 3.5',
-          'client': 'xxx',
-          'category': None,
-          'start_date': '2018-11-14',
-          'close_date': '2018-11-15',
-          'due_date': '2018-11-15',
-          'modified_date': '2018-11-15',
-          'status': 'close'}
-    t2 = t1.copy()
-    t2['subject'] = 'Foo Bar'
-    task_list = [t1, t2]
+@pytest.fixture
+def sample_task():
+    t = {'subject': '[xxx-656] Helping xxx with escalations after xxx-Dev upgrade to 3.5',
+         'client': 'xxx',
+         'category': None,
+         'start_date': '2018-11-14',
+         'close_date': '2018-11-15',
+         'due_date': '2018-11-15',
+         'modified_date': '2018-11-15',
+         'status': 'close'}
+    return t
+
+
+@pytest.fixture
+def setup_initial_task(setup_db, mocker, sample_task):
     db, Task = setup_db
+    db = OpenDB(':memory:')
+    Task.get_or_create(subject='Foo Bar', defaults=sample_task)
     mocker.patch('tasks_collector.tasksdb.api.Task', Task)
+    return db, Task
+
+
+def test_insert_or_updates_tasks(setup_initial_task, sample_task):
+    t2 = sample_task.copy()
+    t2['subject'] = 'Foo Bar'
+    task_list = [sample_task, t2]
+    db, Task = setup_initial_task
     insert_or_updates_tasks(task_list)
     assert len(list(Task.select())) == 2
+
+
+def test_get_all_tasks(setup_initial_task):
+    db, Task = setup_initial_task
+    all_tasks = db.get_all_tasks()
+    assert 'xxx' in all_tasks[0]['subject']
+
+
+def test_cleanup(setup_initial_task, sample_task):
+    db, Task = setup_initial_task
+    t2 = sample_task.copy()
+    t2['subject'] = 'Foo Bar'
+    t2['start_date'] = '2019-01-10'
+    t2['close_date'] = None
+    insert_or_updates_tasks([t2])
+    cleanup('2019-01-20')
+    assert Task.select().order_by(Task.id.desc()).get().close_date == d(2019, 11, 18)
