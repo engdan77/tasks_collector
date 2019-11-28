dc65de01daff97b6f583f9cbf5000520419338ac
Daniel Engvall <daniel@engvalls.eu>
2019-11-28 20:57:24 +0100

    Updating setup to assure build sphinx works finding README.md and requirements.txt

--- setup.py
+++ setup.py
@@ -1,22 +1,36 @@
 from setuptools import setup, find_packages
+import os
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+opt = {
+    'install_requires': {'in': (True, ['requirements.txt', '../requirements.txt']), 'out': None},
+    'long_description': {'in': (False, ['README.md', '../README.md']), 'out': None}
+}
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+for k, v in opt.items():
+    _split, files = v['in']
+    for p in files:
+        try:
+            with open(p) as f:
+                d = f.read()
+                opt[k]['out'] = d.splitlines() if _split else d
+                print(f'Found data for {k}')
+            break
+        except FileNotFoundError:
+            print(f'Unable to find {p} being in {os.getcwd()}')
+    if not opt[k]['out']:
+        raise RuntimeError(f'Abort due to missing data for {k} in setup')
 
 setup(
     name='tasks_collector',
     version='0.0.9',
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=opt['install_requires']['out'],
     url='https://github.com/engdan77/tasks_collector',
     license='MIT',
     author='Daniel Engvall',
     author_email='daniel@engvalls.eu',
     description='A small project for collecting tasks',
-    long_description=long_description,
+    long_description=opt['long_description']['out'],
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
