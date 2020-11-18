aa889b9825882ef44fdfd5e22665e05db31f2009
Daniel Engvall <daniel.engvall@ipsoft.com>
2020-11-18 17:17:13 +0100

    Got further, but caught other place, trying to mock runcmd for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,8 @@ MOCK_MODULES = ['wxpython',
                 'scipy.interpolate',
                 'Gooey',
                 'PySimpleGUIQt',
-                'numpy']
+                'numpy',
+                'runcmd']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
